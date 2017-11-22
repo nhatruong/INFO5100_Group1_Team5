31e02af6e3553bc86153c609e2fc4e2c811feb75
Jin Zezhu <jinzezhu@JindeMacBook-Pro.local>
2017-11-21 23:49:28 -0800

    updated IService with methods relate to inventory

--- src/com/neuSep17/service/IService.java
+++ src/com/neuSep17/service/IService.java
@@ -1,20 +1,25 @@
 package com.neuSep17.service;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
-import com.neuSep17.dto.Dealer;
-import com.neuSep17.dto.Vehicle;
+import com.neuSep17.dto.*;
+
 
 public interface IService {
+	//methods for Dealers
 	ArrayList<Dealer>getDealers();
 	Dealer getADealer(String dealerID);
 	Boolean updateDealer(String dealerID, String oldValue, String newValue);
 	Boolean addDealer(Dealer dealer);
 	Boolean deleteDealer(String dealerID);
-	
+
+	//methods for Vehicles
 	ArrayList<Vehicle> getVehicleList(String dealerID);	
 	Vehicle getVehicleAtADealer(String dealerId, String cardID);
 	Boolean updateVehicle(String dealerId, String vehicleId, String oldValue, String newValue);
-	
-	
+
+	//methods for Inventory
+	Inventory getInventory(String dealerID);
+	Boolean updateInventory(String dealerID, Collection<Vehicle> vehicles);
 }
